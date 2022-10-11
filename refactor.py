8f893cb8b1a22b00f460f302b36da72b7efc5bd3
foster <flynch59@gmail.com>
2022-10-11 15:34:59 -0700

    added classes

--- refactor.py
+++ refactor.py
@@ -1,15 +1,48 @@
-class House:
-    def __init__(self):
+from dataclasses import dataclass
+from sre_parse import _OpInType
+
+@dataclass
+class Expenses:
+    capex: int
+    misc: int
+    mgmt: int
+    water: int
+    sewer: int
+    gas: int
+    electric: int
+    insurance: int
+    accounting: int
+    legal: int
+
+@dataclass
+class Income:
+    rent:int
+
+@dataclass
+class Listing:
+    pass
+
+
+class House(Expenses, Income):
+    def __init__(self, list_price, sqft):
         self.sqft = sqft
         self.list_price = list_price
+
+class MultiFamily(House):
+    def __init__(self) -> None:
+        self.down_payment_pct = 0.2
+
+class SingleFamily(House):
+    def __init__(self) -> None:
+        self.down_payment_pct = 0.03
 
 
 class Investment(House):
     def __init__(self):
         self.down_payment_pct = 0.2
 
-    def compute_expenses(self):
-        pass
+    def cashflow(self):
+        return self.rent - self.
 
 
 class PersonalProperty(House):
@@ -18,3 +51,4 @@ class PersonalProperty(House):
 
     def compute_expenses(self):
         pass
+
