e51246c713370864edb2476c36b62759d405f655
Foster Lynch <foster.lynch@crowe.com>
2022-10-15 13:31:50 -0700

    Wrote functions for web api data pulling

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
-import requests
 import os
+import sys
+import requests
+from typing import List
+
 from dotenv import load_dotenv
+from urllib.parse import urlparse, ParseResult
 
 load_dotenv()
 apikey = os.getenv("apikey")
@@ -11,47 +15,87 @@ headers = os.getenv("headers")
 get_main_info_url = os.getenv("get_main_info_url")
 get_price_info_url = os.getenv("get_price_info_url")
 
-querystring = {"location": address}
 
 headers = {"X-RapidAPI-Key": apikey, "X-RapidAPI-Host": apihost}
 
-# response = requests.request(
-#     "GET", initial_url, headers=headers, params=querystring
-# )
+# def lookup_properties_by_address(address):
+
+#     querystring = {"location": address}
+
+#     response = requests.request(
+#         "GET", initial_url, headers=headers, params=querystring
+#     )
+
+#     propertyId = response.json()["payload"]["exactMatch"]["id"]
+#     propertyId = propertyId.split("_")[1]
+
+#     querystring = {"propertyId": propertyId, "listingId": propertyId}
+
+#     response = requests.request(
+#         "GET", get_main_info_url, headers=headers, params=querystring
+#     )
+#     return response.json()["payload"]
 
 
-# propertyId = response.json()["payload"]["exactMatch"]["id"]
-# propertyId = propertyId.split("_")[1]
-
-# querystring = {"propertyId": propertyId, "listingId": propertyId}
-
-# # response = requests.request(
-# #     "GET", get_main_info_url, headers=headers, params=querystring
-# # )
-
-# response = requests.request(
-#     "GET", get_price_info_url, headers=headers, params=querystring
-# )
+def _get_address_from_url(url: ParseResult) -> List:
+    return url.path.split("/")[1:4]
 
 
-# houseinfo = response.json()["payload"]["publicRecordsInfo"]
-# priceinfo = response.json()["payload"]["propertyHistoryInfo"]
-# print(
-#     response.json()["payload"]["propertyHistoryInfo"]["events"][0][
-#         "price", "mlsDescription", "eventDescription"
-#     ]
-# )
-
-from urllib.parse import urlparse
-
-metadata = {"isRedfinUrl": None}
+def _get_property_id_from_url(url: ParseResult) -> str:
+    return url.path.split("/")[-1]
 
 
-def get_price_from_url(url):
+def _fetch_from_url(propertyId) -> requests.models.Response:
+    querystring = {"propertyId": propertyId, "listingId": propertyId}
+    response = requests.request(
+        "GET", get_price_info_url, headers=headers, params=querystring
+    )
+    print(type(response))
+    return response
+
+
+def get_price_info(response):
+    payload = response.json()["payload"]["propertyHistoryInfo"]["events"][0]
+    price = payload["price"]
+    mlsDescription = payload["mlsDescription"]
+    eventDescription = payload["eventDescription"]
+    return price, mlsDescription, eventDescription
+
+
+def get_property_details(response) -> dict:
+    houseinfo = response.json()["payload"]["publicRecordsInfo"]["basicInfo"]
+    return houseinfo
+
+
+def get_taxes(response) -> dict:
+    taxes = response.json()["payload"]["publicRecordsInfo"]["taxInfo"]
+    return taxes
+
+
+def fetch_data_with_url(url: str) -> dict:
     url = urlparse(url)
-    metadata.update({"isRedfinUrl": "redfin" in url.netloc})
-    print(url)
-
-get_price_from_url(
-    "https://www.redfin.com/NY/Rochester/1064-Lake-Ave-14613/home/92804703"
-)
+    assert "redfin" in url.netloc  # supporting only redfin for now
+    metadata = {}
+    address = _get_address_from_url(url)
+    propertyId = _get_property_id_from_url(url)
+    response = _fetch_from_url(propertyId)
+    try:
+        assert response.status_code == 200
+    except AssertionError as err:
+        sys.exit(err)
+    price, mlsDescription, eventDescription = get_price_info(response)
+    houseinfo = get_property_details(response)
+    taxInfo = get_taxes(response)
+    metadata.update(
+        {
+            "isRedfinUrl": "redfin" in url.netloc,
+            "address": address,
+            "propertyId": propertyId,
+            "price": price,
+            "mlsDescription": mlsDescription,
+            "eventDescription": eventDescription,
+            "taxInfo": taxInfo,
+            "houseinfo": houseinfo,
+        }
+    )
+    return metadata
