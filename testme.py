d8d7653ab2b6ac2ecb604ac18c6bfbc6c15bfd2a
fosterlynch <flynch59@gmail.com>
2023-06-13 13:47:35 -0700

    cleanup

--- testme.py
+++ testme.py
@@ -5,30 +5,38 @@ from api import RedFin
 from taxes import find_tax_rate
 import sqlite3
 import json
-import logging
 
 
-with open("devurls.json", "r") as J:
+def calculate_annual_property_taxes(
+    assessed_value: float, tax_rate: float
+) -> float:
+    return assessed_value * 0.6 * tax_rate
+
+
+with open("devurls.json", "r") as J:  # noqa: W1514
     urls = json.load(J)
+
 for url in urls:
     redfin_webdata = fetch_data_with_url(url)
 
     conn = sqlite3.connect("taxrates.sqlite")
     taxrate = find_tax_rate(conn, redfin_webdata)
-    print(taxrate)
-    # print(response.json())
-    webvalues = Webvalues(  # why pass it into another dataclass?d
+
+    annual_taxes = calculate_annual_property_taxes(
+        redfin_webdata.price, taxrate
+    )
+
+    webvalues = Webvalues(  # why pass it into another dataclass?
         list_price=redfin_webdata.price,
         property_type=redfin_webdata.property_type,
         investment_type="pure_investment",
+        max_down_payment=45000,
         rentroll=[0],
         insurance=53,
-        taxes=(redfin_webdata.price * 0.6 * taxrate)
-        / 12,  # the .6 denotes assessed value
+        taxes=annual_taxes / 12,
         hoa=0,
         interest_rate=0.07,
     )
 
     house = House(webvalues=webvalues)
-    print("address", redfin_webdata.address)
     house.analyze()
